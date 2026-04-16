7f9859016d7a042d5f865f3129994f8b1aecbfb1
Micah Marino <36608767+glassedgamer@users.noreply.github.com>
2026-04-15 20:56:24 -0400

    2 Players
    Added two crosshairs, and allowed both to shoot stuff and destroy enemies

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,36 +3,84 @@ using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
-
+    [Header("Values")]
     public float damage = 10f;
     public float range = 100f;
+    public float crosshairSpeed = 200f;
 
+    [Header("Crosshairs")]
+    public RectTransform crosshairP1;
+    public RectTransform crosshairP2;
+    Vector2 moveInput01;
+    Vector2 moveInput02;
+
+    [Header("Camera")]
     public Camera mainCam;
 
+    [Header("Layers")]
     public LayerMask enemyLayer;
 
 
-    //Calls the shoot function when the input action is called
-    public void OnShoot(InputAction.CallbackContext context)
+    private void Update()
     {
-        Shoot();
+        CrosshairMovement();
+    }
+    void CrosshairMovement()
+    {
+        Vector2 movementP01 = moveInput01 * crosshairSpeed * Time.deltaTime;
+        crosshairP1.anchoredPosition += movementP01;
+
+        Vector2 movementP02 = moveInput02 * crosshairSpeed * Time.deltaTime;
+        crosshairP2.anchoredPosition += movementP02;
     }
 
-    void Shoot()
+    //Calls the shoot function when the input action is called
+    public void OnShootP01(InputAction.CallbackContext context)
     {
-        RaycastHit hit;
-        //Ray ray = new Ray(mainCam.transform.position, mainCam.transform.forward);
-        //Ray ray = new Ray(mainCam.transform.position, mainCam.transform.forward);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        print("P1 shoot");
+        Shoot(crosshairP1);
+    }
 
-        if (Physics.Raycast(ray, out hit, range))
+    //Calls the shoot function when the input action is called
+    public void OnShootP02(InputAction.CallbackContext context)
+    {
+        Shoot(crosshairP2);
+        print("P2 shoot");
+    }
+
+
+    void Shoot(RectTransform crosshair)
+    {
+        print("2 Shoot 2 Nite");
+
+        RaycastHit hit;
+
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(null, crosshair.position);
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+        if (Physics.Raycast(ray, out hit, range, enemyLayer))
         {
             GameObject objHit = hit.transform.gameObject;
+
+            print(objHit.name);
 
             if(objHit.tag == "Enemy")
             {
                 Destroy(objHit);
             }
         }
+    }
+
+    public void OnMoveP01(InputAction.CallbackContext context)
+    {
+        print("P1 movement");
+        moveInput01 = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveP02(InputAction.CallbackContext context)
+    {
+        print("P1 movement");
+        moveInput02 = context.ReadValue<Vector2>();
     }
 }
