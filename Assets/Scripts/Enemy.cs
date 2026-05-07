4dd2fbcce11abe54cd2c58570c31b9db3b5da0f4
mle-4 <mle4@student.fitchburgstate.edu>
2026-05-07 16:11:30 -0400

    Adding remaining assets

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,15 +5,17 @@ public class Enemy : MonoBehaviour
 
     GameManager gm;
 
+    GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(player.transform);
     }
 }
