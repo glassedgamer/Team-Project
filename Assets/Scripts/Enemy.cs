45b057c20e38061e28dcc5280081389e133f7747
glassedgamer <glassedgamer05@gmail.com>
2026-05-12 08:23:19 -0400

    Gorilla Prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    GameManager gm;
+    GameObject player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(player.transform);
     }
 }
