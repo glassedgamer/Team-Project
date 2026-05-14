7e691a7cf8aa2435d8ef16c8dfff116f5cd22ac0
glassedgamer <glassedgamer05@gmail.com>
2026-05-14 11:52:09 -0400

    final stuff and bug fixes

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -21,4 +21,12 @@ public class MainMenuController : MonoBehaviour
             SceneManager.LoadScene("TeamProjectLevel");
         }
     }
+
+    public void OnQuit(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Application.Quit();
+        }
+    }
 }
